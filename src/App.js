37b55e40fe3f3b14121ab8c272617d16d7c6239d
“Victoria240” <taiwovictoria024@gmail.com>
2023-08-16 01:17:41 +0100

    used React Router library to set up routes for each section of the to-do website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  // Define states for the list of todos and loading state
-  const [todoList, setTodoList] = useState([]); // Initialize todoList state with an empty array
-  const [isLoading, setIsLoading] = useState(true); // Initialize isLoading state as true
-  const [appState, setAppState] = useState ('')
+  // Define the initial app state
+  const [appState, setAppState] = useState({
+    todoList: [],
+    isLoading: true,
+  });
 
   async function fetchData() {
-    // Define options for the API request
     const options = {
       method: 'GET',
       headers: {
@@ -17,11 +18,9 @@ function App() {
       },
     };
 
-    // Construct the URL for the API
     const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
 
     try {
-      // Fetch data from the API using the constructed URL and options
       const response = await fetch(url, options);
 
       if (!response.ok) {
@@ -30,29 +29,28 @@ function App() {
 
       const data = await response.json();
 
-      // Transform API data.records into todo objects
       const todos = data.records.map((record) => ({
         id: record.id,
         title: record.fields.title,
       }));
 
-      // Update the todoList state with fetched todos
-      setTodoList(todos);
-      setIsLoading(false); // Set isLoading to false after data fetch
+      setAppState({
+        ...appState,
+        todoList: todos,
+        isLoading: false,
+      });
 
     } catch (error) {
       console.error('Error fetching data:', error.message);
     }
   }
 
-  // Fetch data when the component mounts
   useEffect(() => {
     fetchData();
-  }, []);
+  }); // No need to add fetchData to dependency array
 
   async function handleAddTodo(newTodo) {
     try {
-      // Construct URL for POST request to add a new todo
       const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
 
       const response = await fetch(url, {
@@ -71,38 +69,51 @@ function App() {
       }
 
       const data = await response.json();
-      // Update the entire state object
-      setAppState({
-        todoList: [...appState.todoList, { id: data.id, title: newTodo.title }],
-        isLoading: false,
-      });
 
+      setAppState({
+        ...appState,
+        todoList: [...appState.todoList, { id: data.id, title: newTodo.title }],
+      });
 
     } catch (error) {
       console.error('Error adding todo:', error.message);
     }
   }
 
-  // Function to remove a todo by ID
   function removeTodo(id) {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(updatedTodoList);
+    const updatedTodoList = appState.todoList.filter((todo) => todo.id !== id);
+    setAppState({
+      ...appState,
+      todoList: updatedTodoList,
+    });
   }
 
   return (
-    <>
-      <h1>Todo List</h1>
-      {/* Pass the handleAddTodo function to the AddTodoForm component */}
-      <AddTodoForm onAddTodo={handleAddTodo} />
-      {/* Conditional rendering based on isLoading state */}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        // Pass the todoList and removeTodo function to the TodoList component
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Define a Route for the root path ("/") */}
+        <Route
+          path="/"
+          element={
+            /* Render the TodoList component with props */
+            <TodoList
+              todoList={appState.todoList} // Pass the todoList state as a prop
+              onRemoveTodo={removeTodo}    // Pass the removeTodo function as a prop
+            />
+          }
+        />
+        {/* Define a Route for the "/new" path */}
+        <Route
+          path="/new"
+          element={
+            /* Render the AddTodoForm component with props */
+            <AddTodoForm onAddTodo={handleAddTodo} /> // Pass the handleAddTodo function as a prop
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
