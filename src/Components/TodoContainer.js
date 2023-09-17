ba0a5317f5b7debd9bbfdec0f3dc02cf4d9e28a2
“Victoria240” <taiwovictoria024@gmail.com>
2023-09-17 21:23:24 +0100

    corrected the propType for todoList to an array

--- src/Components/TodoContainer.js
+++ src/Components/TodoContainer.js
@@ -1,73 +1,74 @@
 // TodoContainer.js
-import React, { useEffect, useState } from 'react';
-import TodoList from './TodoList';
-
+import React, { useEffect, useState } from "react";
+import TodoList from "./TodoList";
 
 function TodoContainer() {
-    // State to hold the list of todos and loading status
-    const [todoList, setTodoList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  // State to hold the list of todos and loading status
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    // New async function to fetch data from the API
-    async function fetchData() {
-        // Define options for the API request
-        const options = {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-            },
-        };
+  // New async function to fetch data from the API
+  async function fetchData() {
+    // Define options for the API request
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
+    };
 
-        // Construct the URL for the API
-        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    // Construct the URL for the API
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
 
-        try {
-            const response = await fetch(url, options);
+    try {
+      const response = await fetch(url, options);
 
-            if (!response.ok) {
-                console.log('Airtable error response:');
-                console.log(response);
-                throw new Error(`Error: ${response.status}`);
-            }
+      if (!response.ok) {
+        console.log("Airtable error response:");
+        console.log(response);
+        throw new Error(`Error: ${response.status}`);
+      }
 
-            // Parse the response JSON
-            const data = await response.json();
+      // Parse the response JSON
+      const data = await response.json();
 
-            // Transform API data.records into todo objects
-            const todos = data.records.map((record) => ({
-                id: record.id,
-                title: record.fields.title,
-            }));
+      // Transform API data.records into todo objects
+      const todos = data.records.map((record) => ({
+        id: record.id,
+        title: record.fields.title,
+      }));
 
-            // Update todoList and isLoading states
-            setTodoList(todos);
-            setIsLoading(false);
-        } catch (error) {
-            console.log(`Fetch error: ${error.message}`);
-        }
+      // Update todoList and isLoading states
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(`Fetch error: ${error.message}`);
     }
+  }
 
-    // Fetch data on initial render
-    useEffect(() => {
-        fetchData();
-    }, []);
+  // Fetch data on initial render
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    
-    // Define the removeTodo function to remove a todo item
-    function removeTodo(id) {
-        // Filter the todoList array to exclude the todo item with the specified id
-        const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+  // Define the removeTodo function to remove a todo item
+  function removeTodo(id) {
+    // Filter the todoList array to exclude the todo item with the specified id
+    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
 
-        // Update the todoList state with the new array of todos
-        setTodoList(updatedTodoList);
-    }
+    // Update the todoList state with the new array of todos
+    setTodoList(updatedTodoList);
+  }
 
-    return (
-        <>
-            <TodoList todoList={todoList} onRemoveTodo={removeTodo} isLoading={isLoading} />
-            
-        </>
-    );
+  return (
+    <>
+      <TodoList
+        todoList={todoList}
+        onRemoveTodo={removeTodo}
+        isLoading={isLoading}
+      />
+    </>
+  );
 }
 
 export default TodoContainer;
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,38 +1,40 @@
 // TodoList.js
-import React from 'react';
-import TodoListItem from './TodoListItem';
-import styles from './TodoList.module.css'; // Import the CSS module
-import PropTypes from 'prop-types'; // Import PropTypes
+import React from "react";
+import TodoListItem from "./TodoListItem";
+import styles from "./TodoList.module.css"; // Import the CSS module
+import PropTypes from "prop-types"; // Import PropTypes
 
+
+// Destructure the props todoList and onRemoveTodo from the incoming props object
 function TodoList({ todoList, onRemoveTodo }) {
-    // Destructure the props todoList and onRemoveTodo from the incoming props object
-
-    return (
-        <div className={styles.TodoListContainer}>
-            <div className={styles.TodoList}>
-                {/* Render a heading */}
-                <h2 className={styles.ListHeader}>Todo List</h2>
-                <ul className={styles.List}>
-                    {/* Map over the todoList array */}
-                    {todoList.map(todo => (
-
-                        <TodoListItem
-                            key={todo.id} // Assign a unique key to each TodoListItem
-                            todo={todo} // Pass the todo object as a prop to TodoListItem
-                            onRemoveTodo={onRemoveTodo} // Pass the onRemoveTodo function as a prop to TodoListItem
-                        />
-
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.TodoListContainer}>
+      <div className={styles.TodoList}>
+        {/* Render a heading */}
+        <h2 className={styles.ListHeader}>Todo List</h2>
+        <ul className={styles.List}>
+          {/* Map over the todoList array */}
+          {todoList.map((todo) => (
+            <TodoListItem
+              key={todo.id} // Assign a unique key to each TodoListItem
+              todo={todo} // Pass the todo object as a prop to TodoListItem
+              onRemoveTodo={onRemoveTodo} // Pass the onRemoveTodo function as a prop to TodoListItem
+            />
+          ))}
+        </ul>
+      </div>
+      x
+    </div>
+  );
 }
 TodoList.propTypes = {
-
-    todoList: PropTypes.func, // PropTypes for the 'todoList' prop
-    onRemoveTodo: PropTypes.func, // PropTypes for the 'onChange' prop
+    todoList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+        })
+    ).isRequired,// PropTypes for the 'todoList' prop
+  onRemoveTodo: PropTypes.func, // PropTypes for the 'onRemoveTodo' prop
 };
 
 export default TodoList;
-
