10c7080e19358b27063b78fdbe617a2252befa61
“Victoria240” <taiwovictoria024@gmail.com>
2023-09-18 16:54:08 +0100

    memoized fetchData using useCallback, created a sortField variable,updated the API link to sort by Airtable's view order and sortfields, and added toggle button to change sorting order

--- src/Components/TodoContainer.js
+++ src/Components/TodoContainer.js
@@ -1,74 +1,89 @@
 // TodoContainer.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import TodoList from "./TodoList";
 
 function TodoContainer() {
-  // State to hold the list of todos and loading status
-  const [todoList, setTodoList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+    const [todoList, setTodoList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAscending, setIsAscending] = useState(true); // Track sorting order
 
-  // New async function to fetch data from the API
-  async function fetchData() {
-    // Define options for the API request
-    const options = {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-      },
-    };
+    // Memoize the fetchData function using useCallback to ensure a stable reference between renders as long as its dependencies (in this case, isAscending) don't change.
 
-    // Construct the URL for the API
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+    const fetchData = useCallback(async () => {
+        // Define options for the API request
+        const options = {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+            },
+        };
 
-    try {
-      const response = await fetch(url, options);
+        // Construct the URL for the API with ascending or descending order
+        // Update the URL with sort parameters (field and direction)
+        const order = isAscending ? "asc" : "desc";
+        const sortField = "created"; // Change this to the desired sorting field
+        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?sort[0][field]=${sortField}&sort[0][direction]=${order}`;
 
-      if (!response.ok) {
-        console.log("Airtable error response:");
-        console.log(response);
-        throw new Error(`Error: ${response.status}`);
-      }
+        try {
+            const response = await fetch(url, options);
 
-      // Parse the response JSON
-      const data = await response.json();
+            if (!response.ok) {
+                console.log("Airtable error response:");
+                console.log(response);
+                throw new Error(`Error: ${response.status}`);
+            }
 
-      // Transform API data.records into todo objects
-      const todos = data.records.map((record) => ({
-        id: record.id,
-        title: record.fields.title,
-      }));
+            // Parse the response JSON
+            const data = await response.json();
 
-      // Update todoList and isLoading states
-      setTodoList(todos);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(`Fetch error: ${error.message}`);
+            // Transform API data.records into todo objects
+            const todos = data.records.map((record) => ({
+                id: record.id,
+                title: record.fields.title,
+            }));
+
+            // Update todoList and isLoading states
+            setTodoList(todos);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(`Fetch error: ${error.message}`);
+        }
+    }, [isAscending]);
+
+    // Fetch data on initial render and whenever sorting order changes
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
+    // Function to toggle sorting order
+    function toggleSortingOrder() {
+        // Toggle between ascending and descending order
+        setIsAscending(!isAscending);
     }
-  }
 
-  // Fetch data on initial render
-  useEffect(() => {
-    fetchData();
-  }, []);
+    // Define the removeTodo function to remove a todo item
+    function removeTodo(id) {
+        // Filter the todoList array to exclude the todo item with the specified id
+        const updatedTodoList = todoList.filter((todo) => todo.id !== id);
 
-  // Define the removeTodo function to remove a todo item
-  function removeTodo(id) {
-    // Filter the todoList array to exclude the todo item with the specified id
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+        // Update the todoList state with the new array of todos
+        setTodoList(updatedTodoList);
+    }
 
-    // Update the todoList state with the new array of todos
-    setTodoList(updatedTodoList);
-  }
-
-  return (
-    <>
-      <TodoList
-        todoList={todoList}
-        onRemoveTodo={removeTodo}
-        isLoading={isLoading}
-      />
-    </>
-  );
+    return (
+        <>
+            <button onClick={toggleSortingOrder}
+                style={{
+                    margin: "16px",
+                    cursor: "pointer",
+                }}
+            >
+                
+                Toggle Sorting Order: {isAscending ? "Ascending" : "Descending"}
+            </button>
+            <TodoList todoList={todoList} onRemoveTodo={removeTodo} isLoading={isLoading} />
+        </>
+    );
 }
 
 export default TodoContainer;
